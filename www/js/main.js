b3928eff9584e06940dd3e5a233825f2aa5cdd92
debrawr <debrabonnington@hotmail.com>
2016-11-17 19:25:07 -0500

    click to change RAG status

---
+++ www/js/main.js
@@ -0,0 +1,18 @@
+function changeClass() {
+			switch($('#enough_time').attr('class')) {
+				case 'green':
+					$('#enough_time').removeClass();
+					$(this).addClass('amber');
+					break;
+				case 'amber':
+					$('#enough_time').removeClass();
+					$(this).addClass('red');
+					break;
+				default:
+					$('#enough_time').removeClass();
+					$(this).addClass('green');
+					break;
+				
+			}
+			
+		}
