137ab4d64efc6d81241e44d0245e0803283e01d5
avash <avash.rao@gmail.com>
2016-11-22 04:22:14 -0500

    adds dynamic countdown for preboard

--- platforms/browser/www/js/main.js
+++ platforms/browser/www/js/main.js
@@ -15,13 +15,32 @@ function changeClass() {
 	}
 }
 
-function countdown(sec) {
-	$("#till_boarding h3").html(sec-- + " min");
+function till_boarding() {
+	tb = parseInt($("#till_boarding h3 span").html());
+	tg = parseInt($("#till_gate h3 span").html());
+
+	if (tg + 20 == tb) { // go amber
+		$('#enough_time').removeClass().addClass('amber');
+	}
+
+	if (tg == tb) { // go red
+		$('#enough_time').removeClass().addClass('red');
+	}
+
+	if (tb > 0) {
+		$("#till_boarding h3 span").html(tb - 1);
+	}
+}
+
+function till_gate() {
+	cur = parseInt($("#till_gate h3 span").html());
+	if (cur > 0) {
+		$("#till_gate h3 span").html(cur - 1);
+	}
 }
 
 $(document).ready(function(){
 	$('#enough_time').on('click', changeClass);
-	
-	sec = 53;
-	var counter = setInterval(countdown(sec--), 1000);
+	var counter1 = setInterval(till_boarding, 200);
+	var counter2 = setInterval(till_gate, 3000);
 });
